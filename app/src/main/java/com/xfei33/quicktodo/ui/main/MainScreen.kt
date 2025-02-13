4ff79228d6ca915f2517da7c55b9d5bf0201d0c8
xfei33 <3283245558@qq.com>
2025-02-14 00:26:41 +0800

    feat(ui): 新增主界面布局与导航功能

---
+++ app/src/main/java/com/xfei33/quicktodo/ui/main/MainScreen.kt
@@ -0,0 +1,36 @@
+package com.xfei33.quicktodo.ui.main
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.xfei33.quicktodo.components.AppBottomBar
+import com.xfei33.quicktodo.ui.focus.FocusScreen
+import com.xfei33.quicktodo.ui.message.MessageScreen
+import com.xfei33.quicktodo.ui.profile.ProfileScreen
+import com.xfei33.quicktodo.ui.todo.TodoScreen
+
+@Composable
+fun MainScreen() {
+    val navController = rememberNavController() // 使用独立的 NavController 管理主界面导航
+
+    Scaffold(
+        bottomBar = {
+            AppBottomBar(navController = navController) // 在主界面显示 AppBottomBar
+        }
+    ) { innerPadding ->
+        NavHost(
+            navController = navController,
+            startDestination = "todo",
+            modifier = Modifier.padding(innerPadding)
+        ) {
+            composable("todo") { TodoScreen(navController) }
+            composable("focus") { FocusScreen(navController) }
+            composable("message") { MessageScreen(navController) }
+            composable("profile") { ProfileScreen(navController) }
+        }
+    }
+}
