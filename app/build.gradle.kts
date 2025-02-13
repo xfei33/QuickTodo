5b4fff19dfc69df8eca8bf8889c13c8f2d49ae6f
xfei33 <3283245558@qq.com>
2025-02-13 11:13:42 +0800

    UPDATE: 添加依赖

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,4 +56,10 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    implementation(libs.material3)
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
+    implementation(libs.androidx.navigation.compose)
+    implementation(libs.retrofit)
+    implementation(libs.converter.gson)
 }
