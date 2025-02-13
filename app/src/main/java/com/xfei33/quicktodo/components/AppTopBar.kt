e2f3366200cb75cb4ca5ff95fa5a7c25de4f477b
xfei33 <3283245558@qq.com>
2025-02-13 22:24:07 +0800

    feat: 新增应用顶部栏组件

---
+++ app/src/main/java/com/xfei33/quicktodo/components/AppTopBar.kt
@@ -0,0 +1,23 @@
+package com.xfei33.quicktodo.components
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AppTopBar(onSearchClick: () -> Unit) {
+    TopAppBar(
+        title = { Text("QuickTodo") },
+        actions = {
+            IconButton(onClick = onSearchClick) {
+                Icon(Icons.Default.Search, contentDescription = "Search")
+            }
+        }
+    )
+}
