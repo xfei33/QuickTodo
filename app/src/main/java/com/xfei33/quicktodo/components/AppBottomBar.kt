e7952ca6f748cb561489d1f72f94939589c6bdc4
xfei33 <3283245558@qq.com>
2025-02-13 22:23:53 +0800

    refactor(ui): 移动并重构底部导航栏组件

--- app/src/main/java/com/xfei33/quicktodo/components/AppBottomBar.kt
+++ app/src/main/java/com/xfei33/quicktodo/components/AppBottomBar.kt
@@ -1,12 +1,15 @@
-package com.xfei33.quicktodo
+package com.xfei33.quicktodo.components
 
-import androidx.compose.material3.*
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavController
 import androidx.navigation.compose.currentBackStackEntryAsState
+import com.xfei33.quicktodo.R
 
 @Composable
 fun AppBottomBar(navController: NavController) {
