8226a2fb29f6a9eece3456e9cf4d29aa87bb980f
xfei33 <3283245558@qq.com>
2025-02-14 18:35:44 +0800

    feat(core): 添加同步接口的token验证

--- app/src/main/java/com/xfei33/quicktodo/network/ApiService.kt
+++ app/src/main/java/com/xfei33/quicktodo/network/ApiService.kt
@@ -1,9 +1,12 @@
 package com.xfei33.quicktodo.network
 
+import androidx.room.TypeConverters
+import com.xfei33.quicktodo.data.converter.LocalDateTimeConverter
 import com.xfei33.quicktodo.model.Todo
 import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.Header
 import retrofit2.http.POST
 import retrofit2.http.Query
 import java.time.LocalDateTime
@@ -11,6 +14,7 @@ import java.time.LocalDateTime
 data class AuthRequest(val username: String, val password: String)
 data class AuthResponse(val token: String, val userId: Long)
 
+@TypeConverters(LocalDateTimeConverter::class)
 interface ApiService {
     @POST("auth/register")
     suspend fun register(@Body request: AuthRequest): Response<Void>
@@ -18,14 +22,16 @@ interface ApiService {
     @POST("auth/login")
     suspend fun login(@Body request: AuthRequest): Response<AuthResponse>
 
-    @GET("/todos/sync")
+    @GET("todos/sync")
     suspend fun getIncrementalData(
+        @Header("Authorization") token: String,
         @Query ("userId") userId: Long,
         @Query("lastSyncTime") lastSyncTime: LocalDateTime
     ): Response<List<Todo>>
 
-    @POST("/todos/sync")
+    @POST("todos/sync")
     suspend fun uploadIncrementalData(
+        @Header("Authorization") token: String,
         @Query("userId") userId: Long,
         @Body incrementalData: List<Todo>
     ): Response<Void>
