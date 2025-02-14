142ecfce3a399260d0e0e056afa1b17b0678e007
xfei33 <3283245558@qq.com>
2025-02-14 12:20:20 +0800

    feat: 添加ApiService和UserPreferences的依赖注入

--- app/src/main/java/com/xfei33/quicktodo/data/di/AppModule.kt
+++ app/src/main/java/com/xfei33/quicktodo/data/di/AppModule.kt
@@ -1,7 +1,10 @@
 package com.xfei33.quicktodo.data.di
 
 import android.content.Context
+import com.xfei33.quicktodo.data.UserPreferences
 import com.xfei33.quicktodo.data.db.AppDatabase
+import com.xfei33.quicktodo.network.ApiService
+import com.xfei33.quicktodo.network.RetrofitClient
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -20,4 +23,16 @@ object AppModule {
 
     @Provides
     fun provideTodoDao(appDatabase: AppDatabase) = appDatabase.todoDao()
+
+    @Provides
+    @Singleton
+    fun provideApiService(): ApiService {
+        return RetrofitClient.apiService
+    }
+
+    @Provides
+    @Singleton
+    fun provideUserPreferences(@ApplicationContext context: Context): UserPreferences {
+        return UserPreferences(context)
+    }
 }
