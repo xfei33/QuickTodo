323809376aaf599277402b59c5e689417916848b
xfei33 <3283245558@qq.com>
2025-02-18 22:23:56 +0800

    feat(ui): 新增消息顶部栏组件

---
+++ app/src/main/java/com/xfei33/quicktodo/ui/message/MessageTopBar.kt
@@ -0,0 +1,26 @@
+package com.xfei33.quicktodo.ui.message
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MassageTopBar() {
+    TopAppBar(
+        title = {
+            Text(
+                text = "消息",
+                style = MaterialTheme.typography.headlineLarge
+            )
+        }
+    )
+}
+@Preview
+@Composable
+fun PreviewMassageTopBar() {
+    MassageTopBar()
+}
