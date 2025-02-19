da7ca56f6bde64bcbf73b3fa7ac01d6c41259885
xfei33 <3283245558@qq.com>
2025-02-19 14:09:36 +0800

    feat(ui): 添加待办任务通知渠道

--- app/src/main/java/com/xfei33/quicktodo/QuickTodoApplication.kt
+++ app/src/main/java/com/xfei33/quicktodo/QuickTodoApplication.kt
@@ -1,7 +1,30 @@
 package com.xfei33.quicktodo
 
 import android.app.Application
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.os.Build
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class QuickTodoApplication : Application()
+class QuickTodoApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        createNotificationChannels()
+    }
+
+    private fun createNotificationChannels() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val todosChannel = NotificationChannel(
+                "todos_channel",
+                "待办任务通知",
+                NotificationManager.IMPORTANCE_HIGH
+            ).apply {
+                description = "关于待办任务的提醒通知"
+            }
+
+            val notificationManager = getSystemService(NotificationManager::class.java)
+            notificationManager.createNotificationChannel(todosChannel)
+        }
+    }
+}
